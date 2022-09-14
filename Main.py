c6842c7306648677a8f7b35e7d21fb31f49dd126
RandomPro8 <59402575+RandomPro8@users.noreply.github.com>
2022-09-13 21:51:29 -0700

    Better way of editing a row in sheets/reading and writing RFID

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
+import time
 
 
 # define the scope
@@ -21,7 +22,7 @@ client = gspread.authorize(creds)
 sheet = client.open('Test')
 
 # get the first sheet of the Spreadsheet
-sheet_instance = sheet.get_worksheet(1)
+sheet_instance = sheet.get_worksheet(3)
 
 
 # get the total number of columns
@@ -33,9 +34,16 @@ sheet_instance.col_count
 # print(sheet_instance.cell(col=1,row=1))
 ## >> <Cell R2C3 '63881'>
 
-sheet_instance.update_cell(2, 1, "Val1")
-sheet_instance.update_cell(2, 2, "Val2")
-sheet_instance.update_cell(2, 3, "Val3")
+# stime = time.time()
+# for i in range(100):
+#     sheet_instance.update_cell(3, 1, str(i))
+#     print(i)
+# etime = time.time()
+
+# ttime = etime - stime
+# print("Total time: " + str(ttime))
+# sheet_instance.update_cell(4, 1, ("Total time: " + str(ttime)))
+sheet_instance.update('A1:C1', [[0,1,2]])
 
 
 # I found this github repo that has a lot of examples of how to use rfids. I'm not sure if it will work for you, but it's worth a shot.
--- test.py
+++ test.py
@@ -1,74 +1,28 @@
-#A chatbot
+
+#This reads the RFID tag and prints it to the console
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+        id, text = reader.read()
+        print(id)
+        print(text)
+finally:
+        GPIO.cleanup()
 
 
-import random
+#This writes the RFID tag and prints it to the console
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
-# Define a function that returns a random greeting
-def greeting():
-    # Define a list of responses
-    responses = ["Hello, how are you?", "Hi, how are you doing?", "Hello, how are you feeling today?"]
-    # Return a random response
-    return random.choice(responses)
+reader = SimpleMFRC522()
 
-# Define a function that returns a random question
-def question():
-    # Define a list of responses
-    responses = ["What is your name?", "What is your favorite color?", "What is your favorite food?"]
-    # Return a random response
-    return random.choice(responses)
-
-# Define a function that returns a random statement
-def statement():
-    # Define a list of responses
-    responses = ["I like you.", "You are cool.", "You are awesome."]
-    # Return a random response
-    return random.choice(responses)
-
-# Define a function that returns a random response
-def response():
-    # Define a list of responses
-    responses = ["That is interesting.", "Tell me more.", "Why do you think that?"]
-    # Return a random response
-    return random.choice(responses)
-
-# Define a function that returns a random goodbye
-def goodbye():
-    # Define a list of responses
-    responses = ["Goodbye!", "See you later!", "Have a nice day!"]
-    # Return a random response
-    return random.choice(responses)
-
-
-# Define a function that sends a message to the bot
-def send_message(message):
-    # Print user_template including the user_message
-    print(user_template.format(message))
-    # Create responses
-    if message == "hello":
-        response = greeting()
-    elif message == "goodbye":
-        response = goodbye()
-    elif message[-1] == "?":
-        response = question()
-    elif message[-1] == ".":
-        response = statement()
-    else:
-        response = response()
-    # Print the bot template including the bot's response.
-    print(bot_template.format(response))
-
-# Define a function that runs the bot
-def run():
-    # Print the start message
-    print("Hi, I'm a chatbot. I like to chat with people. If you want to exit, type 'goodbye'.")
-    # While the user input is not goodbye, keep chatting
-    while True:
-        message = input("You: ")
-        if message == "goodbye":
-            send_message(message)
-            break
-        else:
-            send_message(message)
-
-# Run the function
-run()
+try:
+        text = input('New data:')
+        print("Now place your tag to write")
+        reader.write(text)
+        print("Written")
+finally:
+        GPIO.cleanup()
