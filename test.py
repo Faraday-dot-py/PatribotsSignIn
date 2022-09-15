b4ff2b4e71b55f5233809bffce8070482ccd4abf
RandomPro8 <59402575+RandomPro8@users.noreply.github.com>
2022-09-14 19:52:17 -0700

    Just a test :)

--- test.py
+++ test.py
@@ -1,28 +1,30 @@
-
-#This reads the RFID tag and prints it to the console
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
-
-reader = SimpleMFRC522()
-
-try:
-        id, text = reader.read()
-        print(id)
-        print(text)
-finally:
-        GPIO.cleanup()
+print("This is a test")
 
 
-#This writes the RFID tag and prints it to the console
-import RPi.GPIO as GPIO
-from mfrc522 import SimpleMFRC522
+# #This reads the RFID tag and prints it to the console
+# import RPi.GPIO as GPIO
+# from mfrc522 import SimpleMFRC522
 
-reader = SimpleMFRC522()
+# reader = SimpleMFRC522()
 
-try:
-        text = input('New data:')
-        print("Now place your tag to write")
-        reader.write(text)
-        print("Written")
-finally:
-        GPIO.cleanup()
+# try:
+#         id, text = reader.read()
+#         print(id)
+#         print(text)
+# finally:
+#         GPIO.cleanup()
+
+
+# #This writes the RFID tag and prints it to the console
+# import RPi.GPIO as GPIO
+# from mfrc522 import SimpleMFRC522
+
+# reader = SimpleMFRC522()
+
+# try:
+#         text = input('New data:')
+#         print("Now place your tag to write")
+#         reader.write(text)
+#         print("Written")
+# finally:
+#         GPIO.cleanup()
