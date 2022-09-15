3f40417c0232b24991f169b9efbc85b83c733ed5
RandomPro8 <59402575+RandomPro8@users.noreply.github.com>
2022-09-14 20:43:50 -0700

    Reading card and writing data to spreadsheet (first)

--- Main.py
+++ Main.py
@@ -1,18 +1,22 @@
-#hostname: patribots-sign-in
-#username: patribots
-#password: patribots
-
+#import dependencies
 import gspread
 import pandas as pd
 from oauth2client.service_account import ServiceAccountCredentials
 import time
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
+#load variables
+reader = SimpleMFRC522()
 
+#----------------------------------------------------------------------------------------#
+
+#connect to spreadsheet
 # define the scope
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
 # add credentials to the account
-creds = ServiceAccountCredentials.from_json_keyfile_name('./roboticsrfidsignin-99cdbd7ce58b.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('./roboticsrfidsignin.json', scope)
 
 # authorize the clientsheet 
 client = gspread.authorize(creds)
@@ -21,31 +25,38 @@ client = gspread.authorize(creds)
 # get the instance of the Spreadsheet
 sheet = client.open('Test')
 
-# get the first sheet of the Spreadsheet
+# get the third sheet of the Spreadsheet
 sheet_instance = sheet.get_worksheet(3)
 
+#----------------------------------------------------------------------------------------#
 
-# get the total number of columns
-sheet_instance.col_count
-## >> 26
+#reader function
+def readCard():
+    try:
+        _, id = reader.read()
+        print(id)
+        return id
+    finally:
+            GPIO.cleanup()
 
+#send data to spreadsheet
+def sendData(id, time):
+    sheet_instance.update('A2:B2', [[id, time]])
+    # print(id)
 
-# get the value at the specific cell
-# print(sheet_instance.cell(col=1,row=1))
-## >> <Cell R2C3 '63881'>
+#----------------------------------------------------------------------------------------#
 
-# stime = time.time()
-# for i in range(100):
-#     sheet_instance.update_cell(3, 1, str(i))
-#     print(i)
-# etime = time.time()
+#main function
+def main():
+    cache = None
+    while True:
+        id = readCard()
+        if id != cache:
+            cache = id
+            sendData(id, time.time())
+            print("Sent data to spreadsheet")
+        time.sleep(1)
+            
 
-# ttime = etime - stime
-# print("Total time: " + str(ttime))
-# sheet_instance.update_cell(4, 1, ("Total time: " + str(ttime)))
-sheet_instance.update('A1:C1', [[0,1,2]])
-
-
-# I found this github repo that has a lot of examples of how to use rfids. I'm not sure if it will work for you, but it's worth a shot.
-# https://github.com/AdamLaurie/RFIDIOt -- This one is more complicated but has a lot to it about rfidot 
-# https://github.com/phyushin/Rfid_Register -- This one is literally someone who made a rfid sign in and out form
+if __name__ == "__main__":
+    main()
